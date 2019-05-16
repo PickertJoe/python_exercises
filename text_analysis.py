08c059ce1ef5662e4aa0fccfe76bd20cbf901a38
Joseph Pickert <Joe@Josephs-MacBook-Air.local>
2019-05-16 11:56:40 -0500

    Creating a new module for word count function

--- text_analysis.py
+++ text_analysis.py
@@ -1,29 +1,23 @@
 # pulling in the text of Alice in Wonderland
 
-with open('pcc/chapter_10/alice.txt') as file_object:
+with open('chapter_10/alice.txt') as file_object:
     contents = file_object.read()
 
 # using the .split() module to approximate the number of words in the text
 words = contents.split()
 num_words = len(words)
 file = "Alice in Wonderland"
-#This method produces the same number of words found in the book exercise
-#print("The manuscript of " + file + " has " + str(num_words) + " words in it.")
+# This method produces the same number of words found in the book exercise
+print("The manuscript of " + file + " has " + str(num_words) + " words in it.")
 
-#defining this function for late use
-def count_words(filename):
-    """Reads in a text file and outputs the number of words it contains"""
-    try:
-        with open(filename) as file_object:
-            contents = file_object.read()
-    except FileNotFoundError:
-        message = "The file could not be found."
-        message += "Check the file's pathway or user entry."
-        print(message)
-    else:
-        #Approximates the word count of the file
-        words = contents.split()
-        num_words = len(words)
-        print("The file " + filename + " has " + str(num_words) + " words in it.")
+# A function to replicate this process was created in the separate file word_count.py
+# Importing this function from the separate module
 
+from word_count import count_words
 
+count_words("chapter_10/alice.txt")
+count_words("chapter_10/moby_dick.txt")
+count_words("chapter_10/siddhartha.txt")
+
+# Calling the function for a nonexistent file to test try-except block
+count_words("chapter_10/crime_and_punishment.txt")
--- word_count.py
+++ word_count.py
@@ -1,15 +1,15 @@
-# a simple function definition to constrain the word count of a text document
+# a module containing a function to count the words in a manuscript
 def count_words(filename):
     """Reads in a text file and outputs the number of words it contains"""
     try:
         with open(filename) as file_object:
             contents = file_object.read()
-        except FileNotFoundError:
-            message = "The file " + " could not be found."
-            message += "Check the file's pathway or user entry."
-            print(message)
-        else:
-            #Approximates the word count of the file
-            words = contents.split()
-            num_words = len(words)
-            print("The file " + filename + " has " + str(num_words) " words in it.")
+    except FileNotFoundError:
+        message = "The file could not be found."
+        message += " Check the file's pathway or user entry."
+        print(message)
+    else:
+        # Approximates the word count of the file
+        words = contents.split()
+        num_words = len(words)
+        print("The file " + filename + " has " + str(num_words) + " words in it.")
