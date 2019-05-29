15b9bf3f1271c50233d731d750ea118b13c5505d
Joseph Pickert <pickertjoe@gmail.com>
2019-05-29 11:20:13 -0600

    Refactoring visualization scripts

--- dice_visualization.py
+++ dice_visualization.py
@@ -8,31 +8,25 @@ from die import Die
 die_1 = Die()
 die_2 = Die()
 
-results = []
 
 # Rolling the die 100 times and storing the results in an array
-for roll in range(1000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
+results = [die_1.roll() + die_2.roll() for value in range(1000)]
 
 # Ensuring that the results were generated -- optional check
 # print(results)
 
 # Conducting a frequency analysis of the results
-frequencies = []
 combined_sides = die_1.sides + die_2.sides
-for value in range(2, combined_sides + 1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in range(2, combined_sides + 1)]
 
 # Ensuring that the frequency analysis was conducted properly -- optional check
-print(frequencies)
+# print(frequencies)
 
 # Creating a simple histogram of the results from this experiment
 hist = pygal.Bar()
 
 hist.title = "Results from Rolling Two D6 (n=1000)"
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.x_labels = [value for value in range(2, 13)]
 hist.x_title = "Result"
 hist.y_title = "Result Frequency"
 
--- die_visualization.py
+++ die_visualization.py
@@ -7,21 +7,15 @@ from die import Die
 # Because no secondary argument is passed to the class, the die is assumed to be six sided
 die = Die()
 
-results = []
-
 # Rolling the die 100 times and storing the results in an array
-for roll in range(1000):
-    result = die.roll()
-    results.append(result)
+results = [die.roll() for value in range(1000)]
 
 # Ensuring that the results were generated -- optional check
 # print(results)
 
 # Conducting a frequency analysis of the results
-frequencies = []
-for value in range(1, die.sides + 1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in range(1, die.sides + 1)]
+
 
 # Ensuring that the frequency analysis was conducted properly -- optional check
 print(frequencies)
@@ -30,7 +24,7 @@ print(frequencies)
 hist = pygal.Bar()
 
 hist.title = "Results from Rolling One D6 (n=1000)"
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_labels = [number for number in range(1, 7)]
 hist.x_title = "Result"
 hist.y_title = "Result Frequency"
 
